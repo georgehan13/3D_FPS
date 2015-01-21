becfcc2d5f7286d1bc6308d714508b801b6519a6
george <darkblue_b4@hotmail.com>
2015-01-21 21:24:32 +0900

    마우스 로테이션을 카메라 로테이션으로 연결

--- Assets/Script/MouseLook.cs
+++ Assets/Script/MouseLook.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MouseLook : MonoBehaviour {
+public class MouseLook : MonoBehaviour 
+{
+	public float sensitivity = 700.0f;
+	float rotationX;
+	float rotationY;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Update()
+	{
+		float mouseMoveValueX = Input.GetAxis ("Mouse X");
+		float mouseMoveValueY = Input.GetAxis ("Mouse Y");
+
+		Debug.Log ("X :" + mouseMoveValueX);
+
+		rotationY += mouseMoveValueX * sensitivity * Time.deltaTime;
+		rotationX += mouseMoveValueY * sensitivity * Time.deltaTime;
+
+		//Debug.Log ("X :" + rotationX);
+		//Debug.Log ("Y :" + rotationY);
+        
+        rotationY %= 360;
+		rotationX %= 360;
+
+
+		transform.eulerAngles = new Vector3(-rotationX, rotationY, 0.0f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
