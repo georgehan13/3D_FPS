229aa8e61906ce9b352ec1aaf13e3a9fd0fa6c85
george <darkblue_b4@hotmail.com>
2015-01-21 21:51:04 +0900

    카메라 로테이션 제한

--- Assets/Script/MouseLook.cs
+++ Assets/Script/MouseLook.cs
@@ -12,19 +12,33 @@ public class MouseLook : MonoBehaviour
 		float mouseMoveValueX = Input.GetAxis ("Mouse X");
 		float mouseMoveValueY = Input.GetAxis ("Mouse Y");
 
-		Debug.Log ("X :" + mouseMoveValueX);
+		//Debug.Log ("X :" + mouseMoveValueX);
 
 		rotationY += mouseMoveValueX * sensitivity * Time.deltaTime;
 		rotationX += mouseMoveValueY * sensitivity * Time.deltaTime;
 
+		//Debug.Log ("dTime :" + Time.deltaTime);
 		//Debug.Log ("X :" + rotationX);
 		//Debug.Log ("Y :" + rotationY);
         
         rotationY %= 360;
 		rotationX %= 360;
 
+		Debug.Log ("Rotation X : "+ rotationX);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
+		/*
+		if( rotationX > 40.0f)
+			rotationX = 40.0f;
+
+		if( rotationX < -40.0f)
+			rotationX = -40.0f;
+		*/
+
+		rotationX = Mathf.Clamp(rotationX, -40.0f, 40.0f); // !!! same as if
 
 		transform.eulerAngles = new Vector3(-rotationX, rotationY, 0.0f);
+
+
 	}
 
 }
