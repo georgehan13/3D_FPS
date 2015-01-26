83966fd5819bd90bc5b32ad1b6c8629c87f1983c
george <darkblue_b4@hotmail.com>
2015-01-26 21:21:59 +0900

    add fireManager script // Layer Collision Matrix

---
+++ Assets/Script/FireManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireManager : MonoBehaviour 
+{
+	public Transform cameraTransform;
+	public GameObject fireObject;
+	public float power = 20.0f;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonDown("Fire1"))
+		{
+			GameObject obj = Instantiate (fireObject) as GameObject;
+
+			obj.transform.position = transform.position;
+			obj.rigidbody.velocity = cameraTransform.forward * power;
+		}
+	
+	}
+}
