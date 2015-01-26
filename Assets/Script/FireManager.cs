7823cab03abd61d1014cf56dfd8a8bc4e6328086
george <darkblue_b4@hotmail.com>
2015-01-26 22:15:03 +0900

    collision detection // fixed Timestep

--- Assets/Script/FireManager.cs
+++ Assets/Script/FireManager.cs
@@ -5,7 +5,9 @@ public class FireManager : MonoBehaviour
 {
 	public Transform cameraTransform;
 	public GameObject fireObject;
+	public Transform firePosition;
 	public float power = 20.0f;
+	public float ballRotation = Random.Range(10.0f, 20.0f);
 
 	
 	// Update is called once per frame
@@ -15,8 +17,9 @@ public class FireManager : MonoBehaviour
 		{
 			GameObject obj = Instantiate (fireObject) as GameObject;
 
-			obj.transform.position = transform.position;
+			obj.transform.position = firePosition.position;
 			obj.rigidbody.velocity = cameraTransform.forward * power;
+			obj.rigidbody.rotation = cameraTransform.forward * ballRotation;
 		}
 	
 	}
