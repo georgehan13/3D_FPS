b0927d4b34e6ba3bea6b141a0464b313bd2fa7e6
george <darkblue_b4@hotmail.com>
2015-01-28 21:19:59 +0900

    boom process script 추가

---
+++ Assets/Script/BoomProcess.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoomProcess : MonoBehaviour 
+{
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log ("Collision Object Name:" + collision.gameObject.name);
+	}
+
+}
