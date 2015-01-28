a393e95549e2171c9874b533317fa9ade8f3d1d6
george <darkblue_b4@hotmail.com>
2015-01-28 22:04:46 +0900

    폭탄 컬리젼시 오브젝트 지움 파티클 발생

--- Assets/Script/BoomProcess.cs
+++ Assets/Script/BoomProcess.cs
@@ -3,9 +3,25 @@ using System.Collections;
 
 public class BoomProcess : MonoBehaviour 
 {
+	public GameObject particleObject;
+
 	void OnCollisionEnter(Collision collision)
 	{
 		Debug.Log ("Collision Object Name:" + collision.gameObject.name);
+
+		GameObject particleObj = Instantiate (particleObject) as GameObject;
+		
+		particleObj.transform.position = transform.position;
+
+		Destroy( gameObject );
 	}
+
+
+	/*
+	void OnCollisionExit(Collision collision)
+	{
+		Debug.LogWarning("Exit Object Name:" + collision.gameObject.name);
+	}
+	*/
 
 }
---
+++ Assets/Script/myTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myTest : MonoBehaviour 
+{
+
+	void OnCollisionStay(Collision collision) 
+	{
+		if (collision.rigidbody)
+			collision.rigidbody.AddForce(Vector3.up * 15);
+		
+	}
+}
