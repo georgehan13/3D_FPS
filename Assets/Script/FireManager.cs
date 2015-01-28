8b427577242755c23ccfec6e9aacb62c80a28f11
george <darkblue_b4@hotmail.com>
2015-01-28 21:12:21 +0900

    bazooka 추가

--- Assets/Script/FireManager.cs
+++ Assets/Script/FireManager.cs
@@ -7,7 +7,7 @@ public class FireManager : MonoBehaviour
 	public GameObject fireObject;
 	public Transform firePosition;
 	public float power = 20.0f;
-	public float ballRotation = Random.Range(-20.0f, 20.0f);
+
 
 
 	
@@ -21,6 +21,8 @@ public class FireManager : MonoBehaviour
 			obj.transform.position = firePosition.position;
 			obj.rigidbody.velocity = cameraTransform.forward * power;
 			obj.rigidbody.angularVelocity = new Vector3 (Random.Range(-180.0f, 180.0f),Random.Range(-180.0f, 180.0f),Random.Range(-180.0f, 180.0f));
+
+			//obj.rigidbody.AddTorque(Random.Range(-180.0f, 180.0f),Random.Range(-180.0f, 180.0f),Random.Range(-180.0f, 180.0f));
 		
 		}
 	
