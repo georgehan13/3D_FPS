c33624295b8f90b62c454f0db7632277afa52814
george <darkblue_b4@hotmail.com>
2015-02-02 20:29:52 +0900

    스파이더 상태(열거형)

---
+++ Assets/Script/SpiderScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderScript : MonoBehaviour 
+{
+	enum SPIDERSTATE
+	{
+		IDLE = 0,
+		MOVE,
+		ATTACK,
+		DAMAGE,
+		DEAD,
+	}
+
+	SPIDERSTATE spiderState = SPIDERSTATE.IDLE;
+
+	void Update()
+	{
+		switch(spiderState)
+		{
+		case SPIDERSTATE.IDLE:
+			{
+
+			}
+			break;
+		case SPIDERSTATE.MOVE:
+			{
+
+			}
+			break;
+		case SPIDERSTATE.ATTACK:
+			{
+
+			}
+			break;
+		case SPIDERSTATE.DAMAGE:
+			{
+
+			}
+			break;
+		case SPIDERSTATE.DEAD:
+			{
+
+			}
+			break;
+
+
+		}
+	}
+}
