9d7e68ca04ac4bec9a6785b10ac71be23f8b4677
george <darkblue_b4@hotmail.com>
2015-02-02 21:34:38 +0900

    스파이더가 플레이어를 찾고, 그 방향으로 움직임

--- Assets/Script/SpiderScript.cs
+++ Assets/Script/SpiderScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class SpiderScript : MonoBehaviour 
 {
+
 	enum SPIDERSTATE
 	{
 		IDLE = 0,
@@ -14,17 +15,77 @@ public class SpiderScript : MonoBehaviour
 
 	SPIDERSTATE spiderState = SPIDERSTATE.IDLE;
 
+	
+	float stateTime = 0.0f;
+	public float idleStateMaxTime = 2.0f;
+
+	Transform target;
+
+	public float speed = 5.0f;
+	public float rotationSpeed = 10.0f;
+	public float attackableRange = 2.5f;
+
+	CharacterController characterController;
+
+
+	void InitSpider()
+	{
+		spiderState = SPIDERSTATE.IDLE;
+		animation.Play ("iddle");
+	}
+	
+
+	void Awake()
+	{
+		InitSpider();
+	}
+
+
+	void Start()
+	{
+		target = GameObject.Find ("Player").transform;
+		characterController = GetComponent<CharacterController>();
+	}
+
+
+
 	void Update()
 	{
 		switch(spiderState)
 		{
 		case SPIDERSTATE.IDLE:
 			{
+				stateTime += Time.deltaTime;
+				if(stateTime > idleStateMaxTime)
+				{
+				 	stateTime = 0.0f;
+					spiderState = SPIDERSTATE.MOVE;
+				}
 
 			}
 			break;
-		case SPIDERSTATE.MOVE:
+			case SPIDERSTATE.MOVE:
 			{
+				animation.Play ("walk");
+
+				float distance = (target.position - transform.position).magnitude;
+
+				if(distance<attackableRange)
+				{
+					spiderState = SPIDERSTATE.ATTACK;
+				}
+				else
+				{
+					Vector3 dir = target.position - transform.position;
+					dir.y = 0.0f;
+					dir.Normalize();
+					characterController.SimpleMove(dir*speed); // simpleMove - no Y move
+
+					transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), rotationSpeed * Time.deltaTime);
+				}
+
+
+
 
 			}
 			break;
@@ -47,4 +108,7 @@ public class SpiderScript : MonoBehaviour
 
 		}
 	}
+
+
+
 }
