309e2fac6f09d3d3948be9aa0c57d6818c583685
George Han <darkblue_b4@hotmail.com>
2015-02-02 18:53:36 +0900

    150202_homework

--- Assets/Script/BoomProcess.cs
+++ Assets/Script/BoomProcess.cs
@@ -5,13 +5,32 @@ public class BoomProcess : MonoBehaviour
 {
 	public GameObject particleObject;
 
+	//homework add 0202
+	public GameObject wallParticleObject; // for Link in Inspector Tab
+
 	void OnCollisionEnter(Collision collision)
 	{
 		Debug.Log ("Collision Object Name:" + collision.gameObject.name);
 
-		GameObject particleObj = Instantiate (particleObject) as GameObject;
+		/* homework disable 0202
+		GameObject particleObj = Instantiate (particleObject) as GameObject; //see gameobject name!!!
 		
 		particleObj.transform.position = transform.position;
+		*/
+
+		//homework add 0202
+		if (collision.gameObject.name.Contains ("Wall")) 
+		{
+			GameObject wallParticleObj = Instantiate (wallParticleObject) as GameObject;
+			wallParticleObj.transform.position = transform.position;
+		}
+
+		else
+		{
+			GameObject particleObj = Instantiate (particleObject) as GameObject;
+			
+			particleObj.transform.position = transform.position;
+		}
 
 		Destroy( gameObject );
 	}
