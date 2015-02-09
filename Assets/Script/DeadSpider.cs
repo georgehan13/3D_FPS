0530acd2e4a2d61d8d3caff258aa8a3bff38a5f1
george <darkblue_b4@hotmail.com>
2015-02-09 20:56:49 +0900

    Spider 죽은 다음에 아래로 사라지게

---
+++ Assets/Script/DeadSpider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadSpider : MonoBehaviour 
+{
+	public float downSpeed = 0.5f;
+
+
+	void Start () 
+	{
+		StartCoroutine(DeadProcess());
+	
+	}
+
+	IEnumerator DeadProcess()
+	{
+		yield return new WaitForSeconds(2.0f);
+
+		while(transform.position.y > -2.0f)
+		{
+			Vector3 temp = transform.position;
+			temp.y -= downSpeed * Time.deltaTime;
+			transform.position = temp;
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		Destroy(gameObject);
+	}
+
+}
